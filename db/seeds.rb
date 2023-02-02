60e481cf7d2547a33160a15b60c9aad682c14ccf
Ryan Mansfield <ryan.mansfield@launchpadlab.com>
2023-02-02 11:08:35 -0500

    Add Todo Model, and seed DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+raise 'SEEDING NOT ALLOWED IN PRODUCTION' if Rails.env.production?
+
+Todo.destroy_all
+
+Todo.create! [
+  {title: "Make Bed", description: "Make upon waking up", completed: false, due_date: Time.now},
+  {title: "Walk Dog", description: "Don't forget leash and bags", completed: false, due_date: Time.now},
+  {title: "Brush Teeth", description: "Wait till post coffee for this one", completed: false, due_date: Time.now}
+]
