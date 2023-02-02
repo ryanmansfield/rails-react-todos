e1251024ebda5a6cb9c0f1cbc4cdb9a8beba74a0
Ryan Mansfield <ryan.mansfield@launchpadlab.com>
2023-02-02 11:26:13 -0500

    Add index and show actions

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::TodosController < ApplicationController
+  before_action :set_todo, only: [:show]
+
+  def index
+    todos = Todo.all
+    render json: todos
+  end
+
+  def show
+    if @todo
+      render json: @todo
+    else
+      render json: @todo.errors
+    end
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
 end
