63cd5696b29c901e9360349809c7eee2f6829d85
Ryan Mansfield <ryan.mansfield@launchpadlab.com>
2023-02-06 14:09:03 -0500

    Todo Show page content added

--- app/javascript/main/todos/views/TodoShow.js
+++ app/javascript/main/todos/views/TodoShow.js
@@ -13,8 +13,19 @@ function TodoShow() {
     .catch(err => console.log(err))
   }, []);
   return(
-    <div>
-      <h1>{todo.title}</h1>
+    <div className="todo-card">
+      <div>
+        <h1>{todo.title}</h1>
+      </div>
+      <div>
+        <span>Completed: {todo.completed ? "True" : "False"}</span>
+      </div>
+      <div>
+        <span>Description: {todo.description}</span>
+      </div>
+      <div>
+        <span>Due: {todo.due_date}</span>
+      </div>
     </div>
   )
 }
