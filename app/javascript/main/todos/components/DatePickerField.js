ed307a17a0fe4514533508d97ade5f264fffb267
Ryan Mansfield <ryan.mansfield@launchpadlab.com>
2023-02-06 17:43:52 -0500

    Fix datepickerfield

--- app/javascript/main/todos/components/DatePickerField.js
+++ app/javascript/main/todos/components/DatePickerField.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
-// import "react-datepicker/dist/react-datepicker.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 const DatePickerField = ({ name, value, onChange }) => {
   const [dueDate, setDueDate] = useState(value);
@@ -9,7 +9,7 @@ const DatePickerField = ({ name, value, onChange }) => {
       selected={dueDate} 
       onChange={val => { 
         onChange(name, val);
-        setStartDate(val); 
+        setDueDate(val); 
       }}
     />
   );
